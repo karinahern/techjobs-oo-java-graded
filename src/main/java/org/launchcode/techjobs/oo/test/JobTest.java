d4024a48118b7b0b71c415368238cbd123e84bb3
Karina Hernandez <kmhernandez000@gmail.com>
2022-09-30 10:19:22 -0500

    Used unit testing to verify the constructors and equals methods for the Job class

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,51 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    private Job job1, job2, job3, job4, job5;
 
+    @Before
+    public void createJob(){
+       job1 = new Job();
+       job2 = new Job();
+       job3 = new Job("Product tester", new Employer("ACME"),
+               new Location("Desert"), new PositionType("Quality control"),
+               new CoreCompetency("Persistence"));
+       job4 = new Job("Front End Developer", new Employer("Boeing"),
+               new Location("St.Louis"), new PositionType("Developer"),
+               new CoreCompetency("Optimization"));
+       job5 = new Job("Front End Developer", new Employer("Boeing"),
+               new Location("St.Louis"), new PositionType("Developer"),
+               new CoreCompetency("Optimization"));
+    }
+
+    @Test
+    public void testSettingJobId(){
+        assertNotEquals(job1.getId(), job2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        assertEquals(job3.getName(), "Product tester");
+        assertEquals(job3.getEmployer().getValue(), "ACME");
+        assertEquals(job3.getLocation().getValue(), "Desert");
+        assertEquals(job3.getPositionType().getValue(), "Quality control");
+        assertEquals(job3.getCoreCompetency().getValue(), "Persistence");
+    }
+
+    @Test
+    public void testJobsForEquality(){
+        assertFalse(job4.equals(job5));
+    }
 }
